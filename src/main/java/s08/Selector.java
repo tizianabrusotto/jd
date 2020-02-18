2836d7cabf2f64793e30bda3eafb5b33717910db
Manny <egalli64@gmail.com>
2020-02-18 11:07:33 +0100

    robustness

--- src/main/java/s08/Selector.java
+++ src/main/java/s08/Selector.java
@@ -9,63 +9,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Selector {
-    private static final String URL = "jdbc:mysql://localhost:3306/me?serverTimezone=Europe/Rome";
-    private static final String USER = "me";
-    private static final String PASSWORD = "password";
+	private static final String URL = "jdbc:mysql://localhost:3306/me?serverTimezone=Europe/Rome";
+	private static final String USER = "me";
+	private static final String PASSWORD = "password";
 
-    public List<String> getCoderNames() throws SQLException {
-        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT first_name FROM coders ORDER BY 1")) {
-            List<String> results = new ArrayList<>();
-            while (rs.next()) {
-                results.add(rs.getString(1));
-            }
+	public List<String> getCoderNames() throws SQLException {
+		String query = "SELECT first_name FROM coders ORDER BY 1";
 
-            return results;
-        }
-    }
+		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(query)) {
+			List<String> results = new ArrayList<>();
+			while (rs.next()) {
+				results.add(rs.getString(1));
+			}
 
-    public List<Coder> getCoders() throws SQLException {
-        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT first_name, last_name, salary FROM coders ORDER BY 1")) {
-            List<Coder> results = new ArrayList<>();
-            while (rs.next()) {
-                Coder coder = new Coder(rs.getString(1), rs.getString(2), rs.getInt(3));
-                results.add(coder);
-            }
+			return results;
+		}
+	}
 
-            return results;
-        }
-    }
+	public List<Coder> getCoders() throws SQLException {
+		String query = "SELECT first_name, last_name, salary FROM coders ORDER BY 1";
 
-    public List<Coder> getCodersBySalary(double lower) throws SQLException {
-        String query = "SELECT first_name, last_name, salary FROM coders WHERE salary >= " + lower + " ORDER BY 3 DESC";
+		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(query)) {
+			List<Coder> results = new ArrayList<>();
+			while (rs.next()) {
+				Coder coder = new Coder(rs.getString(1), rs.getString(2), rs.getInt(3));
+				results.add(coder);
+			}
 
-        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(query)) {
-            List<Coder> results = new ArrayList<>();
-            while (rs.next()) {
-                results.add(new Coder(rs.getString(1), rs.getString(2), rs.getInt(3)));
-            }
+			return results;
+		}
+	}
 
-            return results;
-        }
-    }
+	public List<Coder> getCodersBySalary(double lower) throws SQLException {
+		String query = "SELECT first_name, last_name, salary FROM coders WHERE salary >= " + lower + " ORDER BY 3 DESC";
 
-    public static void main(String[] args) {
-        try {
-            Selector sample = new Selector();
+		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(query)) {
+			List<Coder> results = new ArrayList<>();
+			while (rs.next()) {
+				results.add(new Coder(rs.getString(1), rs.getString(2), rs.getInt(3)));
+			}
 
-            System.out.println("Coder names are: " + sample.getCoderNames());
-            System.out.println("Coders are: " + sample.getCoders());
-            System.out.println("Rich coders are: " + sample.getCodersBySalary(6000));
+			return results;
+		}
+	}
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return;
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			Selector sample = new Selector();
+
+			System.out.println("Coder names are: " + sample.getCoderNames());
+			System.out.println("Coders are: " + sample.getCoders());
+			System.out.println("Rich coders are: " + sample.getCodersBySalary(6000));
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return;
+		}
+	}
 }
