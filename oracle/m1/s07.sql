d6a0cea9d090702198013dab543f8095b1bc045c
Manny <egalli64@gmail.com>
2020-09-09 21:39:51 +0200

    grant select

--- oracle/m1/s07.sql
+++ oracle/m1/s07.sql
@@ -1,7 +1,9 @@
--- run it as root
+-- run this as root
 
--- drop user me;
+-- drop user me cascade;
 
 create user me identified by password account unlock;
 grant connect, resource to me;
+grant select on hr.employees to me;
+
 alter user me quota unlimited on users;
