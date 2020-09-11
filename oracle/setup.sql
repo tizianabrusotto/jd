0553c08e6cd13a52442aef39ab3450aceee417a9
Manny <egalli64@gmail.com>
2020-09-11 12:56:45 +0200

    view

--- oracle/setup.sql
+++ oracle/setup.sql
@@ -4,6 +4,7 @@
 
 create user me identified by password account unlock;
 grant connect, resource to me;
+grant create view to me;
 grant select on hr.employees to me;
 
 alter user me quota unlimited on users;
