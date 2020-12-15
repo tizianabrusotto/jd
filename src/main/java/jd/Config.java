21e9ca129825f42b2dfdf57a6ad7ea7fd8c7e403
Manny <egalli64@gmail.com>
2020-12-15 23:29:30 +0100

    minor cleanup

--- src/main/java/jd/Config.java
+++ src/main/java/jd/Config.java
@@ -2,31 +2,29 @@ package jd;
 
 public interface Config {
     /** MySQL */
-    // public static final String URL = "jdbc:mysql://localhost:3306/me";
-
     static boolean isMySql() {
         return false;
     }
 
-    // or, when something like default-time-zone="+02:00" is not set in my.ini
-    //
+    // public static final String URL = "jdbc:mysql://localhost:3306/me";
+    // or, if in my.ini there is no -> default-time-zone="+02:00"
     // public static final String URL = "jdbc:mysql://localhost:3306/me?serverTimezone=Europe/Rome";
 
     /** Oracle DB */
-    // public static final String URL = "jdbc:oracle:thin:@127.0.0.1:1521/xe";
-
     static boolean isOracle() {
         return false;
     }
 
-    /** SQLite */
-    public static final String URL = "jdbc:sqlite:me.db";
+    // public static final String URL = "jdbc:oracle:thin:@127.0.0.1:1521/xe";
 
+    /** SQLite */
     static boolean isSqLite() {
         return true;
     }
 
-    // Access to user/schema me
+    public static final String URL = "jdbc:sqlite:me.db";
+
+    // User access info
     public static final String USER = "me";
     public static final String PASSWORD = "password";
 }
