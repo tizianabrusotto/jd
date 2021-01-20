d607905d9229d70ff5f2a60971d8fdd587690587
Manny <egalli64@gmail.com>
2021-01-20 11:59:09 +0100

    cleanup

--- src/main/java/s14/Main.java
+++ src/main/java/s14/Main.java
@@ -1,14 +1,13 @@
 package s14;
 
-import java.util.List;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import s14.dao.Coder;
 import s14.dao.CoderDao;
 
 public class Main {
-    private static Logger logger = LoggerFactory.getLogger(Main.class);
+    private static Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         CoderDao cd = new CoderDao();
@@ -16,42 +15,38 @@ public class Main {
         // create a new coder
         Coder tom = new Coder(501, "Tom", "Jones", 2000);
         cd.save(tom);
+        System.out.println("Save: " + tom);
 
-        // get a coder
-        Optional<Coder> opt = cd.get(501);
-        if (opt.isPresent()) {
-            System.out.println("Coder 501: " + opt.get());
-
-            Coder coder = opt.get();
+        cd.get(501).ifPresentOrElse(coder -> {
+            System.out.println("Get: " + coder);
             coder.setSalary(coder.getSalary() * 2);
             cd.update(coder);
-        } else {
-            logger.error("Unexpected! Can't get coder 501");
-        }
+            System.out.println("Update salary: " + coder);
+        }, () -> {
+            log.error("Unexpected! Can't get coder 501");
+        });
 
         // rename a coder
         tom.setLastName("Bombadil");
         cd.update(tom);
+        System.out.println("Update renamed: " + tom);
 
-        opt = cd.get(501);
-        if (opt.isPresent()) {
-            System.out.println("Coder 501: " + opt.get());
-        } else {
-            logger.error("Unexpected! Can't get coder 501");
-        }
+        cd.get(501).ifPresentOrElse(coder -> {
+            System.out.println("Get: " + coder);
+        }, () -> {
+            log.error("Unexpected! Can't get coder 501");
+        });
 
         // delete a coder
         cd.delete(501);
 
-        opt = cd.get(501);
-        if (opt.isPresent()) {
-            logger.error("Unexpected! Coder 501 still alive: " + opt.get());
-        } else {
+        cd.get(501).ifPresentOrElse(coder -> {
+            log.error("Unexpected! Coder 501 still alive: " + coder);
+        }, () -> {
             System.out.println("Coder 501 is no more");
-        }
+        });
 
-        // get all coders
-        List<Coder> coders = cd.getAll();
-        System.out.println("Coders: " + coders);
+        System.out.println("All coders");
+        cd.getAll().stream().forEach(System.out::println);
     }
 }
