0fed4b3d03bc7a83a2217a7fdb17b0dff21a5cf2
Emanuele Galli <egalli64@gmail.com>
2021-02-10 11:14:50 +0100

    alias r

--- mySql/m2/s04.sql
+++ mySql/m2/s04.sql
@@ -1,6 +1,6 @@
 -- table alias
 use me;
 
-select r.region_name as "Region Name", r.country_name as country
+select r.region_name as "Region Name", c.country_name as country
 from regions r, countries c
 where r.region_id = c.region_id;
