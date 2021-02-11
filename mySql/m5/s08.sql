372b9781aa3ef35e5d74776f4557d1cea4ecb636
Emanuele Galli <egalli64@gmail.com>
2021-02-11 18:07:51 +0100

    no spoiler

--- mySql/m5/s08.sql
+++ mySql/m5/s08.sql
@@ -16,19 +16,9 @@ create procedure get_coder_salary(
 	where coder_id = p_coder_id;
 end;
 
-create procedure get_department_led(
-	in p_employee_id integer,
-    out p_department_name varchar(30)
-) begin
-	select department_name
-	into p_department_name
-	from departments
-	where manager_id = p_employee_id;
-end;
-
 // delimiter ;
 
-call get_coder_salary(13404, @my_salary);
+call get_coder_salary(103, @my_salary);
 select @my_salary;
 
 -- exercise
