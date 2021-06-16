d11cc0203a8c1ae9e92e301b25d11da2bf8fc74d
Emanuele Galli <egalli64@gmail.com>
2021-06-16 20:05:21 +0200

    review 420

--- mySql/m1/s06.sql
+++ mySql/m1/s06.sql
@@ -2,7 +2,7 @@
 use me;
 
 -- a select
-select region_name
+select name
 from regions
 where region_id = 1;
 
@@ -20,30 +20,39 @@ select manager_id
 from employees;
 
 -- select with change on results
-select job_title, min_salary, min_salary + 2000 as "option 1", min_salary * 3 + 1000 as "option 2"
+select title, min_salary, min_salary + 2000 as "option 1", min_salary * 3 + 1000 as "option 2"
+from jobs;
+
+select title as "Job Title", min_salary + 2000 as "Salary option 1"
 from jobs;
 
 -- alias
-select job_title, min_salary as original, min_salary as salary
+select title, min_salary as "original", min_salary as salary
 from jobs;
 
-select job_title, min_salary + 2000 as "increased min salary"
+select title, min_salary + 2000 as "increased min salary"
 from jobs;
 
 -- dual
-select current_date, 1+2
-from dual;
+select current_date;
+-- from dual;
+
+select sqrt(25);
 
 select 1+2, 3-4, 2*6, 5/2, current_date;
 
 -- concatenation
-select concat(region_id, ": " ,country_id)
+select concat(region_id, ": " ,country_id) as "the countries"
 from countries;
 
 -- limit to get result set with a specified size
 select first_name, last_name
 from employees
 limit 10;
+
+select first_name, last_name
+from employees
+limit 10, 10;
 
 select first_name, last_name
 from employees
