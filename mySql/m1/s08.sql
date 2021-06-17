8c6cbd42521f7866574a81636406460114236df7
Emanuele Galli <egalli64@gmail.com>
2021-06-17 13:35:50 +0200

    review 420

--- mySql/m1/s08.sql
+++ mySql/m1/s08.sql
@@ -2,15 +2,34 @@
 use me;
 
 -- check null
-select employee_id, first_name, last_name
+select employee_id, first_name, last_name, commission_pct
 from employees
-where commission_pct is not null;
+where commission_pct is null
+and last_name = 'King';
+
+select employee_id, first_name, last_name, commission_pct
+from employees
+where commission_pct is not null
+and last_name = 'King';
+
+-- !!! WRONG !!! check null
+select employee_id, first_name, last_name, commission_pct
+from employees
+where commission_pct = null
+and last_name = 'King';
+
+-- !!! WRONG !!!
+select employee_id, first_name, last_name, commission_pct
+from employees
+where commission_pct != null
+and last_name = 'King';
 
 -- null in operations
-select first_name, last_name, 12 * salary * commission_pct
-from employees;
-
--- what if null
-select employee_id, last_name, 12 * salary * ifnull(commission_pct, 0)
+select first_name, last_name, salary * commission_pct
 from employees
-where employee_id = 145;
+where last_name = 'King';
+
+-- what if the commission is null?
+select employee_id, last_name, salary * ifnull(commission_pct, 0)
+from employees
+where last_name = 'King';
