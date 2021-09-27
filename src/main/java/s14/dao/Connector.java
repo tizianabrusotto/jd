e9213e2161b542dfaa64e38566926b8f3c8c16e1
Emanuele Galli <egalli64@gmail.com>
2021-09-27 21:08:30 +0200

    minor cleanup

--- src/main/java/s14/dao/Connector.java
+++ src/main/java/s14/dao/Connector.java
@@ -1,17 +1,14 @@
 package s14.dao;
 
+import static jd.Config.PASSWORD;
+import static jd.Config.URL;
+import static jd.Config.USER;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import static jd.Config.*;
-
 public class Connector {
-    private static final Logger log = LogManager.getLogger(Connector.class);
-
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
