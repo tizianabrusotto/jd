db321fd157f6011a70ab9750262ef7efef715ca7
Emanuele <egalli64@gmail.com>
2021-12-15 12:08:32 +0100

    hron review

--- src/main/java/s10/Transactor.java
+++ src/main/java/s10/Transactor.java
@@ -11,15 +11,23 @@ import static jd.Config.*;
 public class Transactor {
     private static final Logger log = LogManager.getLogger(Transactor.class);
 
-    private static final String ALL_CODERS = "SELECT coder_id, first_name, last_name, hire_date FROM coders";
-    private static final String INSERT_CODER = "INSERT INTO coders (first_name, last_name, hire_date, salary) VALUES( ?, ?, ?, ?)";
-    private static final String UPDATE_CODER_NAME = "UPDATE coders SET first_name = ? WHERE first_name = ? AND last_name = ?";
+    private static final String GET_ALL_CODERS = """
+            SELECT employee_id, first_name, last_name, hired
+            FROM employee e JOIN department d USING (department_id)
+            WHERE d.name = 'IT'""";
+    private static final String INSERT_CODER = """
+            INSERT INTO employee (first_name, last_name, phone, hired, job_id, salary, manager_id, department_id) VALUES
+                (?, ?, ?, ?, 15, ?, 103, 6)""";
+    private static final String UPDATE_EMPLOYEE_NAME = """
+            UPDATE employee
+            SET first_name = ?
+            WHERE first_name = ? AND last_name = ?""";
 
     public static void main(String[] args) {
         Transactor transactor = new Transactor();
 
-        transactor.insertCoder("Jim", "Steward", 7000.0);
-        transactor.insertUpdateCoder("John", "Watson", 6500.0);
+        transactor.insertCoder("Jim", "Steward", 9999, 7000.0);
+        transactor.insertUpdateCoder("John", "Watson", 9998, 6500.0);
         transactor.sloppyUpdate("Valli", "Pataballa");
 
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
@@ -34,12 +42,12 @@ public class Transactor {
         StringBuilder sb = new StringBuilder("[");
 
         try (Statement stmt = conn.createStatement(); //
-             ResultSet rs = stmt.executeQuery(ALL_CODERS)) {
+                ResultSet rs = stmt.executeQuery(GET_ALL_CODERS)) {
             while (rs.next()) {
-                sb.append("(").append(rs.getInt("coder_id")).append(": "); // 1
+                sb.append("(").append(rs.getInt("employee_id")).append(": "); // 1
                 sb.append(rs.getString("first_name")).append(" "); // 2
                 sb.append(rs.getString("last_name")).append(" "); // 3
-                sb.append(rs.getDate("hire_date")).append(")"); // 4
+                sb.append(rs.getDate("hired")).append(")"); // 4
             }
         } finally {
             sb.append("]");
@@ -47,13 +55,13 @@ public class Transactor {
         }
     }
 
-    public void insertCoder(String first, String last, double salary) {
+    public void insertCoder(String first, String last, int phone, double salary) {
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
             conn.setAutoCommit(false);
 
             try {
                 System.out.println("Inserting new coder ...");
-                doInsert(conn, first, last, salary);
+                doInsert(conn, first, last, phone, salary);
 
                 doSomethingDangerous();
                 conn.commit();
@@ -70,12 +78,12 @@ public class Transactor {
         }
     }
 
-    public void insertUpdateCoder(String first, String last, double salary) {
+    public void insertUpdateCoder(String first, String last, int phone, double salary) {
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             connection.setAutoCommit(false);
 
             System.out.println("Inserting new coder ...");
-            doInsert(connection, first, last, salary);
+            doInsert(connection, first, last, phone, salary);
 
             System.out.println("Setting savepoint");
             Savepoint savepoint = connection.setSavepoint();
@@ -116,12 +124,13 @@ public class Transactor {
         }
     }
 
-    private void doInsert(Connection connection, String first, String last, double salary) {
+    private void doInsert(Connection connection, String first, String last, int phone, double salary) {
         try (PreparedStatement ps = connection.prepareStatement(INSERT_CODER)) {
             ps.setString(1, first);
             ps.setString(2, last);
-            ps.setObject(3, LocalDate.now());
-            ps.setDouble(4, salary);
+            ps.setInt(3, phone);
+            ps.setObject(4, LocalDate.now());
+            ps.setDouble(5, salary);
             ps.executeUpdate();
             logAllCoders(connection);
         } catch (SQLException se) {
@@ -130,7 +139,7 @@ public class Transactor {
     }
 
     private void doUpdate(Connection connection, String first, String last) {
-        try (PreparedStatement ps = connection.prepareStatement(UPDATE_CODER_NAME)) {
+        try (PreparedStatement ps = connection.prepareStatement(UPDATE_EMPLOYEE_NAME)) {
             ps.setString(1, first + "ny");
             ps.setString(2, first);
             ps.setString(3, last);
