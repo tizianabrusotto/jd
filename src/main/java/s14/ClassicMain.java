ace3ab1f31e8efb88c8332747501f8570a382d6e
Emanuele <egalli64@gmail.com>
2021-12-16 09:47:11 +0100

    Classic and modern main

---
+++ src/main/java/s14/ClassicMain.java
@@ -0,0 +1,57 @@
+package s14;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import s14.dao.Coder;
+import s14.dao.CoderDao;
+
+public class ClassicMain {
+    private static final Logger log = LogManager.getLogger(ClassicMain.class);
+
+    public static void main(String[] args) {
+        CoderDao cd = new CoderDao();
+
+        // create a new coder ...
+        final long id = 501L;
+        cd.save(new Coder(id, "Tom", "Jones", 99_999, 2_000));
+
+        // ... then get it
+        Coder fiveOone = cd.legacyGet(id);
+        if (fiveOone == null) {
+            log.error("Unexpected! Can't get the coder " + id);
+            System.out.println("Coder has not been saved correctly!");
+            return;
+        } else {
+            System.out.println("Get: " + fiveOone);
+        }
+
+        // update coder salary
+        fiveOone.setSalary(fiveOone.getSalary() * 2);
+        cd.update(fiveOone);
+        System.out.println("Update salary: " + fiveOone);
+
+        // rename the coder
+        fiveOone.setLastName("Hollz");
+        cd.update(fiveOone);
+        System.out.println("Update renamed: " + fiveOone);
+
+        // delete the coder
+        cd.delete(id);
+
+        // ensure the coder is actually removed from the database
+        fiveOone = cd.legacyGet(id);
+        if (fiveOone == null) {
+            System.out.println("Coder correctly removed");
+        } else {
+            System.out.println("Unexpected! Coder is still alive: " + fiveOone);
+        }
+
+        System.out.println("All coders");
+        List<Coder> coders = cd.getAll();
+        for (Coder coder : coders) {
+            System.out.println(coder);
+        }
+    }
+}
--- src/main/java/s14/Main.java
+++ src/main/java/s14/Main.java
@@ -11,41 +11,34 @@ public class Main {
     public static void main(String[] args) {
         CoderDao cd = new CoderDao();
 
-        // create a new coder
-        Coder tom = new Coder(501, "Tom", "Jones", 99_999, 2_000);
-        cd.save(tom);
-        System.out.println("Save: " + tom);
+        // create a new coder ...
+        final long id = 501L;
+        cd.save(new Coder(id, "Tom", "Jones", 99_999, 2_000));
 
-        cd.get(501).ifPresentOrElse(coder -> {
+        // ... then get it
+        cd.get(id).ifPresentOrElse(coder -> {
             System.out.println("Get: " + coder);
+
+            // update coder salary
             coder.setSalary(coder.getSalary() * 2);
             cd.update(coder);
             System.out.println("Update salary: " + coder);
+
+            // rename a coder
+            coder.setLastName("Hollz");
+            cd.update(coder);
+            System.out.println("Update renamed: " + coder);
         }, () -> {
             log.error("Unexpected! Can't get coder 501");
             System.out.println("No coder to work with!");
         });
 
-        // rename a coder
-        tom.setLastName("Hollz");
-        cd.update(tom);
-        System.out.println("Update renamed: " + tom);
+        // delete the coder
+        cd.delete(id);
 
-        cd.get(501).ifPresentOrElse(coder -> System.out.println("Get: " + coder),
-                () -> log.error("Unexpected! Can't get coder 501"));
-
-        Coder coder501 = cd.legacyGet(501);
-        if (coder501 != null) {
-            System.out.println("Legacy get: " + coder501);
-        } else {
-            log.error("Unexpected! Can't get coder 501");
-        }
-
-        // delete a coder
-        cd.delete(501);
-
-        cd.get(501).ifPresentOrElse(coder -> log.error("Unexpected! Coder 501 still alive: " + coder),
-                () -> System.out.println("Coder 501 is no more"));
+        // ensure the coder is actually removed from the database
+        cd.get(id).ifPresentOrElse(coder -> log.error("Unexpected! Coder is still alive: " + coder),
+                () -> System.out.println("Coder correctly removed"));
 
         System.out.println("All coders");
         cd.getAll().forEach(System.out::println);
