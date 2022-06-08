242608c8472e08914ff001587d76fe1d93c8e67d
Emanuele <egalli64@gmail.com>
2022-06-08 12:47:22 +0200

    improved example

--- src/main/java/com/example/jd/s05/PreparedRemover.java
+++ src/main/java/com/example/jd/s05/PreparedRemover.java
@@ -5,17 +5,21 @@ import java.sql.*;
 import static com.example.jd.Config.*;
 
 public class PreparedRemover {
-    private static final String DELETE_SERVICE_BY_NAME = "DELETE FROM service WHERE name = ?";
+    private static final String DELETE_SERVICE_BY_NAME = """
+            DELETE FROM service
+            WHERE name = ? and location_id = ?""";
 
     public static void main(String[] args) {
         // this info should come from user
         String name = "Smith's Barn";
+        int location = 18;
 
-        System.out.printf("Deleting service named %s, if exists%n", name);
+        System.out.printf("Deleting service named %s, location %d, if exists%n", name, location);
 
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-             PreparedStatement stmt = conn.prepareStatement(DELETE_SERVICE_BY_NAME)) {
+                PreparedStatement stmt = conn.prepareStatement(DELETE_SERVICE_BY_NAME)) {
             stmt.setString(1, name);
+            stmt.setInt(2, location);
             int lines = stmt.executeUpdate();
             System.out.printf("Delete executed, %d lines affected%n", lines);
         } catch (SQLException se) {
